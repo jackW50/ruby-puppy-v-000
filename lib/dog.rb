c3a7954175e3624e8e3916a1ce5276899955ae04
John Werenski <jtwerenski@gmail.com>
2018-09-19 21:04:33 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  attr_reader :name
+  @@all = []
+ 
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all. do |dogs|
+      puts dogs.name
+    end 
+  end 
+  
+  def self.clear_all
+    @@all.clear 
+  end 
+  
+end 
